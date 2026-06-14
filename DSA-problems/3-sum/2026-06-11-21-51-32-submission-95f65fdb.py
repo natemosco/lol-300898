afbdfde4f5839c3610f6b507b4ba37a238195dee
Nathaniel Mosco <natemosco@gmail.com>
2026-06-14 14:58:58 -0400

    sync 3-sum submission 2026-06-11T21:51:32.143Z

---
+++ DSA-problems/3-sum/2026-06-11-21-51-32-submission-95f65fdb.py
@@ -0,0 +1,8 @@
+def three_sum(arr, w):
+  for index i
+  for index j > i
+    for index k > j
+      if elements i, j, and k add to w
+        return true
+return false
+# return a naive solution should be successful
